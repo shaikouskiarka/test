9c3e24a4828a8f950a52746320413d4ccaa164ee
shaikouskiarka <arkadiy92@mail.ru>
2014-12-12 00:07:14 +0300

    add

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.arkadiy.clojure/fplabs "1.0.0-SNAPSHOT"
+  :description "Project for Functional programming tasks"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :resource-paths ["resources"]
+  :aot :all
+  :main org.arkadiy.clojure.fplabs.core)
